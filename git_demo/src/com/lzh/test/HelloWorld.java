f0efa950e2da50a37d0cb47a71cde29fd47f60ea
luozehua <luozehua@wamingtech.com>
2019-02-21 21:25:47 +0800

    添加HelloWorld类

---
+++ git_demo/src/com/lzh/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lzh.test;
+
+/**
+ * 测试
+ * 
+ * @author Administrator
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
